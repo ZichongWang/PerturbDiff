362b7c6b740a32ef2411285735d9efc591b3b5d0
zichongwang <zichongwang04@gmail.com>
2026-03-24 14:15:20 +0800

    calcel batch, enable more stable training

--- replogle_from_scratch.sh
+++ replogle_from_scratch.sh
@@ -4,9 +4,9 @@ COMMON_TRAIN_RUNTIME="
 trainer.devices=[0]
 trainer.use_distributed_sampler=false
 data.normalize_counts=10
-trainer.max_steps=300000
+trainer.max_steps=200000
 lightning.callbacks.checkpoint.save_top_k=-1
-trainer.limit_val_batches=5
+trainer.limit_val_batches=32
 lightning.ema.decay=0.99
 lightning.ema.update_steps=10
 cov_encoding.batch_encoding=onehot
@@ -23,9 +23,9 @@ data.embed_key=X_hvg
 "
 
 COMMON_REPLOGLE_TRAIN="
-optimization.micro_batch_size=128
+optimization.micro_batch_size=2048
 data.use_cell_set=32
-optimization.optimizer.lr=0.0001
+optimization.optimizer.lr=0.0005
 cov_encoding.replogle_gene_encoding=genept
 "
 
@@ -65,9 +65,21 @@ data.prefetch_factor=12
 trainer.val_check_interval=1.0
 lightning.callbacks.checkpoint.every_n_train_steps=10000
 run_name=from_scratch_replogle
-optimization.micro_batch_size=128
 lightning.logger.project=diffusion_pertub
-lightning.logger.name=from_scratch_replogle_low_lr
+lightning.logger.name=replogle_no_batch_bs2048
+"
+
+CKPT_NAMING="
+lightning.callbacks.checkpoint.dirpath='${trainer.default_root_dir}/${run_name}/ckpts'
+lightning.callbacks.checkpoint.monitor=validation_validation_loss_epoch
+lightning.callbacks.checkpoint.filename='${run_name}-{epoch}-{step}-{validation_validation_loss_epoch:.4f}'
+"
+
+
+FAKE_BATCH="
+data.dataset_path=/home/zichong/fork/PerturbDiff/data/perturb_data/finetune_data/nadig_processed_data/replogle_fake_batch.h5ad
+data.perturbseq_batch_col=fake_batch
+data.skip_cached_indices=true
 "
 
 CUDA_VISIBLE_DEVICES=0 python ./src/apps/run/rawdata_diffusion_training.py \
@@ -76,5 +88,7 @@ $COMMON_MODEL_2000 \
 $COMMON_SCRATCH_DATA \
 $COMMON_REPLOGLE_TRAIN \
 $SCRATCH_REPLOGLE_EXTRA \
+$FAKE_BATCH \
+$CKPT_NAMING
 
 
