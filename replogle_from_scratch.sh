3de65083b8b5ffa0de9b8e732cdf9ad68e4d3e6c
zichongwang <zichongwang04@gmail.com>
2026-04-07 10:47:30 +0800

    Change validation batch setting

--- replogle_from_scratch.sh
+++ replogle_from_scratch.sh
@@ -5,7 +5,7 @@ trainer.devices=[0]
 trainer.use_distributed_sampler=false
 data.normalize_counts=10
 trainer.max_steps=200000
-lightning.callbacks.checkpoint.save_top_k=-1
+lightning.callbacks.checkpoint.save_top_k=5
 trainer.limit_val_batches=32
 lightning.ema.decay=0.99
 lightning.ema.update_steps=10
@@ -25,7 +25,7 @@ data.embed_key=X_hvg
 COMMON_REPLOGLE_TRAIN="
 optimization.micro_batch_size=512
 data.use_cell_set=32
-optimization.optimizer.lr=0.0005
+optimization.optimizer.lr=0.0003
 cov_encoding.replogle_gene_encoding=genept
 "
 
@@ -62,11 +62,11 @@ SCRATCH_REPLOGLE_EXTRA="
 data=replogle_finetune
 data.num_workers=16
 data.prefetch_factor=12
-trainer.val_check_interval=0.5
+trainer.val_check_interval=1.0
 lightning.callbacks.checkpoint.every_n_train_steps=10000
 run_name=from_scratch_replogle
 lightning.logger.project=diffusion_pertub
-lightning.logger.name=new_metrics_v1
+lightning.logger.name=new_val_batch_v1
 "
 
 CKPT_NAMING="
